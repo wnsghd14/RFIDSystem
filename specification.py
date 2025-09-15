4dd541d23376794d50b35ff7abd6813dab605ce7
Joonhong choi <108641819+wnsghd14@users.noreply.github.com>
2025-09-15 17:06:46 +0900

    Delete specification.py

--- specification.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from inventory2.backend.models.specification import Specification
-
-
-class SpecificationsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Specification
-        fields = "__all__"
